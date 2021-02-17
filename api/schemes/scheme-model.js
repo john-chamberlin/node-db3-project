1c0cb703499d735500295d1519fe6b27f59e927a
John Chamberlin <johnchamberlin00@gmail.com>
2021-02-17 16:29:56 -0600

    mvp done i think

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,63 @@
 // scheme-model
+const db = require('../../data/db-config')
+
+function find() {
+	return db('schemes')
+}
+
+function findById(id) {
+	const schemeObject = db('schemes').where('id', id).first()
+	if (!schemeObject) {
+		return Promise.resolve(null)
+	} else {
+		return schemeObject
+	}
+}
+
+function findSteps(id) {
+	return db('schemes as sc')
+		.join('steps as s', 's.scheme_id', 'sc.id')
+		.select('s.id', 'sc.scheme_name', 's.step_number', 's.instructions')
+		.where('sc.id', id)
+		.orderBy('s.step_number')
+}
+
+function add(scheme) {
+	return db('schemes')
+		.insert(scheme)
+		.then(([id]) => {
+			return findById(id)
+		})
+}
+
+function update(changes, id) {
+	return db('schemes')
+		.update(changes)
+		.where('id', id)
+		.then(() => {
+			return findById(id)
+		})
+}
+
+async function remove(id) {
+	const scheme = await findById(id)
+	if (!scheme) {
+		return Promise.resolve(null)
+	} else {
+		return db('schemes')
+			.where('id', id)
+			.del()
+			.then(() => {
+				return scheme
+			})
+	}
+}
+
+module.exports = {
+	find,
+	findById,
+	findSteps,
+	add,
+	update,
+	remove
+}
--- queries.sql
+++ queries.sql
@@ -1,9 +1,30 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+select p.productname, c.categoryname from product as p
+join category as c
+group by productname
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+select id, s.companyname, orderdate from 'order' as o
+join shipper as s
+    on o.shipvia = s.id
+where (orderdate < '2012-08-09')
+order by orderdate asc
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+select p.productname, od.quantity from orderdetail as od
+join product as p
+    on od.productid = p.id
+where od.orderid = 10251
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+select
+ id as 'Order ID',
+ c.companyname as 'Company Name',
+ e.lastname as 'Employee Last Name'
+    from 'order' as o
+join employee as e
+    on e.id = o.employeeid
+join customer as c
+    on o.customerid = c.id
